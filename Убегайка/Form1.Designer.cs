f62bdb27c903f15b0eee7c44d3ceda522860db93
Vadymkon <vadykakonon@gmail.com>
2023-07-23 01:27:36 +0300

    First commit

---
+++ Убегайка/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace Убегайка
+{
+    partial class Убегайка
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Убегайка));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(415, 369);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "TakeME";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button1_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(281, 223);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(452, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Скорее всего в этой игре нельзя победить :)";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(13, 13);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(18, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.Visible = false;
+            this.checkBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.checkBox1_MouseMove);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button2.Location = new System.Drawing.Point(13, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(18, 22);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "R";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "ПЕРЕЗАГРУЗКА";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "<-";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button3.Location = new System.Drawing.Point(13, 100);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(18, 22);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "S";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "SONG";
+            // 
+            // Убегайка
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 753);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Убегайка";
+            this.Opacity = 0.9D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Убегайка  -  Донаты: 4149 4991 5443 8277";
+            this.Load += new System.EventHandler(this.Убегайка_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Убегайка_MouseClick);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Убегайка/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Убегайка
+{
+    public partial class Убегайка : Form
+    {
+        int speed = 10, smth = 1;
+
+
+       // System.Drawing.Color[] arr = { "red","white" ,"blue", "yellow", "control"};
+        public Убегайка()
+        {
+            InitializeComponent();
+            WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer();
+            wplayer.URL = @"mp3\background.mp3";
+            wplayer.controls.play();
+        }
+
+        public void winning()
+        {
+            WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer();
+            wplayer.URL = @"mp3\win.mp3";
+            wplayer.controls.play();
+        }
+        void button1_Click(object sender, EventArgs e)
+        {
+            winning();
+            button1.Enabled = false;
+            BackColor = Color.Gray;
+            button1.Text = "Поздравляем! Вы победили!";
+            label1.Text = "Вау! Вы молодец, это было непросто!\nЧтобы попробовать снова, поймайте этот текст.";
+            label1.Left = Left / 2; label1.Top = Top / 2;
+            Thread.Sleep(1000);
+            this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label1_MouseMove);
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            label2.Text = "ПЕРЕЗАГРУЗКА\n(также скорость уменьшает)";
+
+        }
+
+        void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (button1.Left < 12) button1.Left = Width - 200; if (button1.Left > Width - 200) button1.Left = 12;
+            if (button1.Top < 12) button1.Top = Height - 200; if (button1.Top > Height - 200) button1.Top = 12;
+        }
+
+        public void checkboxsound()
+        {
+            WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer();
+            wplayer.URL = @"mp3\labelchange.mp3";
+            wplayer.controls.play();
+        }
+        public void song()
+        {
+            WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer();
+            wplayer.URL = @"mp3\background.mp3";
+            wplayer.controls.play();
+        }
+        void label1_Click(object sender, EventArgs e)
+        {
+            button1.Left = Width / 2; button1.Top = Height / 2;
+            button1.Width -= 3; button1.Height -= 3;
+            button1.Enabled = true;
+            button1.Text = "TAKEME";
+            label1.Left = Left / 2; label1.Top = Top / 2;
+            label1.Text = "Нажав на текст вы сделаете режим сложнее.\nНо его то поймать ещё нужно))0)";
+           
+            WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer();
+            wplayer.URL = @"mp3\labelchange.mp3";
+            wplayer.controls.play();
+            
+            checkBox1.Text = "Кнопка уменьшена на 3 пикселя";
+            checkBox1.Visible = true;
+        }
+
+        void label1_MouseMove(object sender, MouseEventArgs e)
+        {
+            WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer();
+            wplayer.URL = @"mp3\label.mp3";
+            wplayer.controls.play();
+            var Arraylist = new Color[] { Color.Red, Color.Blue, Color.Green, Color.Purple, Color.Azure, Color.Aqua, Color.Chocolate };
+            List<Color[]> color = new List<Color[]>();
+
+            Random rand = new Random();
+            smth++;
+            if (smth % 20 == 0) speed++; //увеличиваем скорость
+            BackColor = Arraylist[rand.Next(7)]; //меняем на случайный цвет
+            label1.BackColor = Arraylist[rand.Next(7)]; //меняем на случайный цвет
+            //меняем положение
+            if (rand.Next(2) == 1) label1.Top += rand.Next(2, speed);
+            else label1.Top += rand.Next(-speed, -2);
+            if (rand.Next(2) == 1) label1.Left += rand.Next(2, speed);
+            else label1.Left += rand.Next(-speed, -2);
+            if (label1.Left < 12) label1.Left = Width - 200; if (label1.Left > Width - 200) label1.Left = 12;
+            if (label1.Top < 12) label1.Top = Height - 200; if (label1.Top > Height - 200) label1.Top = 12;
+        }
+
+        void Убегайка_MouseClick(object sender, MouseEventArgs e)
+        {
+            Random rand = new Random();
+            if (rand.Next(10) == 4)
+            {
+                WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer();
+                wplayer.URL = @"mp3\missed.mp3";
+                wplayer.controls.play();
+            }
+            if (button1.Left < 12) button1.Left = Width - 200; if (button1.Left > Width - 200) button1.Left = 12;
+            if (button1.Top < 12) button1.Top = Height - 200; if (button1.Top > Height - 200) button1.Top = 12;
+            if (label1.Left < 12) label1.Left = Width - 200; if (label1.Left > Width - 200) label1.Left = 12;
+            if (label1.Top < 12) label1.Top = Height - 200; if (label1.Top > Height - 200) label1.Top = 12;
+        }
+
+        private void checkBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            checkBox1.Visible = false;
+        }
+
+        private void Убегайка_Load(object sender, EventArgs e)
+        {
+            WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer();
+            wplayer.URL = @"mp3\Start.mp3";
+            wplayer.controls.play();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            speed = 10;
+            button1.Left = Width / 2; button1.Top = Height / 2;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            song();
+        }
+
+        void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer();
+            wplayer.URL = @"mp3\button.mp3";
+            wplayer.controls.play();
+
+            var Arraylist = new Color[] { Color.Red, Color.Blue, Color.Green, Color.Purple, Color.Azure, Color.Aqua, Color.Chocolate };
+        List<Color[]> color = new List<Color[]>();
+
+        Random rand = new Random();
+            smth++; 
+            if (smth % 20 == 0) speed++; //увеличиваем скорость
+            BackColor = Arraylist[rand.Next(7)]; //меняем на случайный цвет
+            button1.BackColor = Arraylist[rand.Next(7)]; //меняем на случайный цвет
+            //меняем положение
+            if (rand.Next(2) == 1) button1.Top += rand.Next(2,speed); 
+                else button1.Top += rand.Next(-speed, -2);
+            if (rand.Next(2) == 1) button1.Left += rand.Next(2, speed);
+                else button1.Left += rand.Next(-speed, -2);
+            if (button1.Left < 12) button1.Left = Width - 200; if (button1.Left > Width-200) button1.Left = 12;
+            if (button1.Top < 12) button1.Top = Height-200; if (button1.Top > Height-200) button1.Top = 12;
+            //Thread.Sleep(1);
+        }
+    }
+}
